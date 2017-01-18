7e04771e5eb9fc37147cfb440c9faf9ad2ab6108
Ole Kristian Ek Hornnes <ole@horneks.com>
2017-01-19 00:39:17 +0100

    Added default value feature to merge tags
    A Kolon between the tag-word and the close brackets delimit the tag from its default value.
    
    Signed-off-by: Ole Kristian Ek Hornnes <ole@horneks.com>

--- index.php
+++ index.php
@@ -6,10 +6,12 @@ $file = file_get_contents('mergedoc.rtf');
 $mergetext = str_replace("\\", "€€", $file); 
 
 // The five-part regex expression (carefully crafted) :-)
-$regex = '/<<((?:€€[a-z0-9]*|\}|\{|\s)*)([a-z0-9.\-\+_æøåÆØÅA-Z]*)((?:€€[a-z0-9]*|\}|\{|\s)*)([a-z0-9.\-\+_æøåÆØÅA-Z]*)((?:€€[a-z0-9]+|\}|\{|\s)*)>>/'; 
+// $regex = '/<<((?:€€[a-z0-9]*|\}|\{|\s)*)([a-z0-9.\-\+_æøåÆØÅA-Z]*)((?:€€[a-z0-9]*|\}|\{|\s)*)([a-z0-9.\-\+_æøåÆØÅA-Z]*)((?:€€[a-z0-9]+|\}|\{|\s)*)>>/'; 
+// New seven part regex with default value detection
+$regex2 = '/<<((?:€€[a-z0-9]*|\}|\{|\s)*)([a-z0-9.\-\+_æøåÆØÅA-Z]*)((?:€€[a-z0-9]*|\}|\{|\s)*)([a-z0-9.\-\+_æøåÆØÅA-Z]*)((?:€€[a-z0-9]*|\}|\{|\s)*)(?:\s*:(.*?)\s*)?((?:€€[a-z0-9]*|\}|\{|\s)*)>>/';
 
 // Find all the matches in it....
-preg_match_all($regex,$mergetext, $out, PREG_SET_ORDER);
+preg_match_all($regex2,$mergetext, $out, PREG_SET_ORDER);
 
 // Lets see the result
 var_dump($out); 
@@ -18,13 +20,20 @@ foreach ($out as $match) {
 	$whole_tag = $match[0]; // The part we actually replace. 
 	$start = $match[1]; // The start formatting that has been injected in our tag, if any
 	$tag = $match[2]; // The tag word itself. 
-	$end = $match[3]; // The end formatting that might be inserted. 
+	if (($match[4].$match[6]) != "") { //some sec-part tag or default value?
+		$end = $match[5]; // The end formatting that might be inserted. 
+		if ($end == "") {
+			$end = $match[7]; // No end in 5, we try 7. 
+		}
+	} else {
+		$end = $match[3]; // No second tag or default value, we find end in match-3 
+	}
+	
 	$secPartTag = $match[4]; // Do we have inserted some formatting inside the tag word too ? 
 	if ($secPartTag != "") {
 		$tag .= $secPartTag; // Put it together with the tag word. 
-		$end = $match[5]; // We ignore the match-3, since this then becomes the formatting injected inside the word
-						// and lets the new end formatting be match-5 instead. 
 	}
+	$default_value = $match[6]; 
 	
 	// Simple selection of what we do with the tag. 
 	switch ($tag) {
@@ -34,8 +43,15 @@ foreach ($out as $match) {
 		case 'SOMEOTHERTAG':
 			$txt = "SOME OTHER TEXT XX"; 
 			break; 
+		case 'THISHASDEFAULT':
+			$txt = ""; 
+			break; 
+		
 		default:
 			$txt = "NOTAG"; 
+	}
+	if ($txt == "") {
+		$txt = $default_value; 
 	}
 	// Create RTF Line breaks in text, if any. 
 	$txt = str_replace(chr(10), chr(10)."\\line", $txt); 
